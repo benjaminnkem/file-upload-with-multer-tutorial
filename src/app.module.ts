076bfac61d3b994bf94706c500e12c6931b04d61
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-05-28 12:27:25 +0100

    Revert "feat: file upload with multer setup"
    This reverts commit 149d3ced77d53ba56b5231f9f9dc2342e919f2d0.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [ProfileModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/profile/profile.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ProfileService {
-  uploadProfilePicture() {
-    return 'This action adds a new profile picture';
-  }
-}
